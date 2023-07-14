0f421e226617b63b7d6b5f52a330f5c97a84ad49
Hamza Ahmad <iiamhamxa@gmail.com>
2023-07-14 13:21:29 +0500

    Paypal Node Api

---
+++ public/script.js
@@ -0,0 +1,36 @@
+paypal
+  .Buttons({
+    createOrder: function () {
+      return fetch("/create-order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          items: [
+            {
+              id: 2,
+            },
+          ],
+        }),
+      })
+        .then((res) => {
+          if (res.ok) return res.json();
+          return res.json().then((json) => Promise.reject(json));
+        })
+        .then(({ id }) => {
+          return id;
+        })
+        .catch((e) => {
+          console.error(e.error);
+        });
+    },
+    onApprove: function (data, actions) {
+      return actions.order.capture().then((details) => {
+        const name = details.payer.name.given_name;
+        console.log(details);
+        alert(`Transaction completed by ${name}`);
+      });
+    },
+  })
+  .render("#paypal");
---
+++ server.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+
+const paypal = require("@paypal/checkout-server-sdk");
+const Environment =
+  process.env.NODE_ENV === "production"
+    ? paypal.core.LiveEnvironment
+    : paypal.core.SandboxEnvironment;
+const paypalClient = new paypal.core.PayPalHttpClient(
+  new Environment(
+    process.env.PAYPAL_CLIENT_ID,
+    process.env.PAYPAL_CLIENT_SECRET
+  )
+);
+
+const sepupBalance = new Map([
+  [1, { price: 100, name: "Balance $100" }],
+  [2, { price: 200, name: "Balance $100" }],
+]);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    paypalClientId: process.env.PAYPAL_CLIENT_ID,
+  });
+});
+
+app.post("/create-order", async (req, res) => {
+  const request = new paypal.orders.OrdersCreateRequest();
+  const total = req.body.items.reduce((sum, item) => {
+    return sum + sepupBalance.get(item.id).price;
+  }, 0);
+  console.log(req.body);
+  request.prefer("return=representation");
+  request.requestBody({
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        reference_id: "d9f80740-38f0-11e8-b467-0ed5f89f718b",
+        amount: {
+          currency_code: "USD",
+          value: total.toString(),
+        },
+        shipping: {
+          address: {
+            address_line_1: "123 Shipping Street",
+            admin_area_2: "Shipping City",
+            admin_area_1: "Shipping State",
+            postal_code: "12345",
+            country_code: "US",
+          },
+        },
+      },
+    ],
+
+    payment_source: {
+      paypal: {
+        experience_context: {
+          payment_method_preference: "IMMEDIATE_PAYMENT_REQUIRED",
+          payment_method_selected: "PAYPAL",
+          brand_name: "SERPUP",
+          locale: "en-US",
+          landing_page: "LOGIN",
+          user_action: "PAY_NOW",
+          return_url: "http://localhost:3000/",
+          cancel_url: "http://localhost:3000/",
+        },
+      },
+    },
+  });
+
+  try {
+    const order = await paypalClient.execute(request);
+    res.json({ id: order.result.id });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(3000);
